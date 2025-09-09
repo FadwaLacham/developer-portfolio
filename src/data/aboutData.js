bb6bbf6dd8377f647ba1186ecdc8a15ed5ddc9a1
FadwaLacham <128438153+FadwaLacham@users.noreply.github.com>
2025-09-09 21:14:04 +0200

    about me

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name's Jane. I'm a web designer and developer based in Southampton, UK.",
-    description2: "During the day I work as a lead developer at a local agency and in the evening I  work on freelance projects and utilize the time to built my own products. I spent my leisure hours writing articles and poetry. Right now I'm also trying a hand at machine learning and AI. I love to learn and explore new arenas.",
+    description1: "My name is Fadwa Lacham. I'm a Data Science and IoT Engineer, passionate about Artificial Intelligence and innovative technological solutions.",
+    description2: "I specialize in creating intelligent systems and data-driven applications that solve complex problems. With expertise in machine learning, natural language processing, and AI development, I enjoy building innovative solutions that make a real impact. I'm constantly exploring new technologies and approaches in the AI field and also take on freelance projects to apply my skills across diverse challenges.",
     image: 2
 }
